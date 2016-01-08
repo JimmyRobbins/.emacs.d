f12e313fc5a2a712485362fc6ea086854486245a
Jimmy Robbins <jrobbins.trombone@gmail.com>
2016-01-08 00:44:53 -0600

    squash! Added Yassnipets and changed the bell function

--- usr/my-functions.el
+++ usr/my-functions.el
@@ -1,17 +1,17 @@
 ;; Basic Functions defined and/or stolen by me
 
 ;; A Visual bell that actually works
-(setq ring-bell-function '(lambda()
-			    (invert-face 'default)
-			    (sleep-for 0.01)
-			    (invert-face 'default)))
+;; (setq ring-bell-function '(lambda()
+;; 			    (invert-face 'default)
+;; 			    (sleep-for 0.01)
+;; 			    (invert-face 'default)))
 
 ;; ;; A Second Option
-;; (setq ring-bell-function '(lambda ()
-;; 			    (setq original-bg-color (face-attribute 'default :background))
-;; 			    (set-face-background 'default "tomato")
-;; 			    (sleep-for 0.01)
-;; 			    (set-face-background 'default original-bg-color)))
+(setq ring-bell-function '(lambda ()
+			    (setq original-bg-color (face-attribute 'default :background))
+			    (set-face-background 'default "#b58900")
+			    (sleep-for 0.01)
+			    (set-face-background 'default original-bg-color)))
 
 
 (provide 'my-functions)
