155f2392aeca8cf4153bb71068427f831169e664
Jimmy Robbins <jrobbins.trombone@gmail.com>
2016-01-09 11:20:37 -0600

    Further cosmetic changes

--- custom.el
+++ custom.el
@@ -17,10 +17,12 @@
  '(mm-inline-large-images (quote resize))
  '(mouse-wheel-scroll-amount (quote (1)))
  '(ns-pop-up-frames nil)
+ '(scroll-bar-mode nil)
  '(text-scale-mode-step 1.1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Inconsolata"))))
+ '(which-func ((t (:weight bold)))))
--- usr/cosmetics.el
+++ usr/cosmetics.el
@@ -6,18 +6,24 @@
 ;; Font size
 (set-face-attribute 'default nil :height 150) 
 
-;; Always show matching pairs of parenthesis
-(show-paren-mode t)
-
 ;; Get rid of visual clutter
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-no-scroll-bar)
 
+;; Add visual clutter
+
+(show-paren-mode t)
+(global-hl-line-mode 1)
+(setq-default indicate-buffer-boundaries 'left)
+(setq-default indicate-empty-lines +1)
+(display-time)
+(which-function-mode)
+
 ;; Color Themes
 (load-theme 'solarized t)
 (set-frame-parameter nil 'background-mode 'dark)
 (enable-theme 'solarized)
-
+ 
 (provide 'cosmetics)
