540d7edd5740fe034f431a5203674c3ee89d7176
Jimmy Robbins <jrobbins.trombone@gmail.com>
2016-01-22 23:50:41 -0600

    Actually made commits start in insert state!

--- usr/settings-magit.el
+++ usr/settings-magit.el
@@ -1,6 +1,6 @@
 ;; My Settings for Magit
 
-(add-hook 'magit-commit-setup-hook 'evil-insert-state)
+(add-hook 'git-commit-setup-hook 'evil-insert-state)
 
 (define-key magit-popup-mode-map [escape] 'magit-popup-quit)
 
