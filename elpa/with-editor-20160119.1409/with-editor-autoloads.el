df6581e2d69cbca2af8af72df6a6c7431bd4f4cd
Jimmy Robbins <jrobbins.trombone@gmail.com>
2016-06-14 01:09:03 -0500

    upgraded packages!!!

--- elpa/with-editor-20160119.1409/with-editor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; with-editor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22178 60296 508963 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; with-editor-autoloads.el ends here
