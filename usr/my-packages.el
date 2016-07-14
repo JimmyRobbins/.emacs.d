a8287f82e0afb55557bb701b5456e8dc12cf5237
Jimmy Robbins <jrobbins.trombone@gmail.com>
2016-07-14 00:32:13 -0500

    FIXED PACKAGE INSTALL PROMPTING
    it turns out advice can tell if it's advised function was called
    interactively
    who'da thunk...

--- usr/my-packages.el
+++ usr/my-packages.el
@@ -10,6 +10,7 @@
     color-theme
     column-enforce-mode
     company
+    dash-at-point
     diminish
     elisp-slime-nav
     elpy
@@ -30,6 +31,7 @@
     linum-relative
     magit
     multiple-cursors
+    package-utils
     projectile
     rainbow-delimiters
     rainbow-mode
@@ -40,7 +42,6 @@
     undo-tree
     unicode-fonts
     unkillable-scratch
-    xkcd
     yasnippet)
   "My top level packages, ensure these are all installed upon launch")
 
@@ -65,39 +66,30 @@
 
 ;;; Prompt me to update this file whenever I install or delete a package
 (defun prompt-to-record-after-installing (package-name)
-  (if (not (member package-name my-packages))
-      ;; prompt to add the package to my-packages file
-      (if (y-or-n-p (format "%s not in my-packages.el, add it ? " package-name))
-          (progn
-            (find-file "~/.emacs.d/usr/my-packages.el")
-            (goto-char (point-min))
-            (search-forward "my-packages")))))
-
-(advice-add 'package-install :after #'prompt-to-record-after-installing)
-
-(defun prompt-to-record-after-deleting (pkg-desc)
-  (let ((package-name (package-desc-name pkg-desc)))
-    (if (member package-name my-packages)
-        ;; prompt to remove the package from my-packages file
-        (if (y-or-n-p
-             (format "%s is in my-packages.el, remove it ?" package-name))
+  (when (called-interactively-p 'any)
+    (if (not (member package-name my-packages))
+        ;; prompt to add the package to my-packages file
+        (if (y-or-n-p (format "%s not in my-packages.el, add it ? " package-name))
             (progn
               (find-file "~/.emacs.d/usr/my-packages.el")
               (goto-char (point-min))
               (search-forward "my-packages"))))))
 
+(advice-add 'package-install :after #'prompt-to-record-after-installing)
+
+(defun prompt-to-record-after-deleting (pkg-desc)
+  (when (called-interactively-p 'any)
+    (let ((package-name (package-desc-name pkg-desc)))
+      (if (member package-name my-packages)
+          ;; prompt to remove the package from my-packages file
+          (if (y-or-n-p
+               (format "%s is in my-packages.el, remove it ?" package-name))
+              (progn
+                (find-file "~/.emacs.d/usr/my-packages.el")
+                (goto-char (point-min))
+                (search-forward "my-packages")))))))
+
 (advice-add 'package-delete :after #'prompt-to-record-after-deleting)
-
-;;; Don't prompt me to edit if I'm just upgrading
-(defun silence-package-prompting (&rest args)
-  (advice-remove 'package-install 'prompt-to-record-after-installing)
-  (advice-remove 'package-delete 'prompt-to-record-after-deleting))
-(advice-add 'package-menu-mark-upgrades :after 'silence-package-prompting)
-
-(defun resume-package-prompting (&rest args)
-  (advice-add 'package-install :after 'prompt-to-record-after-installing)
-  (advice-add 'package-delete :after 'prompt-to-record-after-deleting))
-(advice-add 'package-menu-execute :before 'resume-package-prompting)
 
 
 (provide 'my-packages)
